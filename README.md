a0465e02b98b7c1ee551d0ee7bb4dcf70ebc1f44
allaev <34710484+allaev@users.noreply.github.com>
2018-04-18 21:00:10 +0500

    add multiple currencies; add materializeCSS

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # btc-currency
+
+JS AJAX Bitcoin Currency Checker
+
+- JavaScript
+- AJAX
+- XHR
+- MaterializeCSS
+- CoinDesk API
--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-var btn = document.querySelector("button");
-var priceDisp = document.querySelector("#price");
-var currency = "USD";
+var span = document.querySelector('#price');
+var btn = document.querySelector('button');
 
-btn.addEventListener("click", function(){
+btn.addEventListener('click', function() {
   var XHR = new XMLHttpRequest();
 
   XHR.onreadystatechange = function() {
-    if(XHR.readyState == 4 && XHR.status == 200) {
-      var data = JSON.parse(XHR.responseText);
-      var price = data.bpi[currency].rate;
-      priceDisp.innerText = price + " " + currency;
-    }
-  }
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      var currency = document.querySelector('input[name=currency]:checked').value;   
 
-  var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
-  XHR.open("GET", url);
+      var res = JSON.parse(XHR.responseText);
+      var price = res.bpi[currency].rate;
+      span.innerHTML = price + ' ' + currency;
+    }
+  };
+  var url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+  XHR.open('GET', url);
   XHR.send();
-})
+});
